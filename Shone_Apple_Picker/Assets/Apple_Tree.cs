c8545040df027353febc0f0b816be00d8f779517
jshone999 <jshone999@gmail.com>
2019-10-15 14:42:06 -0400

    Demo first commit

--- Shone_Apple_Picker/Assets/Apple_Tree.cs
+++ Shone_Apple_Picker/Assets/Apple_Tree.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Apple_Tree : MonoBehaviour
 {
     public GameObject Apple_Prefab;
+    public GameObject Gold_Apple_Prefab;
+
     public float speed = 1f;
     public float leftAndRightEdge = 10f;
     public float chanceToChangeDirections = 0.1f;
@@ -12,16 +14,33 @@ public class Apple_Tree : MonoBehaviour
 
     void Start()
     {
-        Invoke("DropApple", 2f);
+        Invoke("ranDrop", 2f);
     }
 
+    void ranDrop()
+    {
+        if (Random.value * 100 > 10)
+        {
+            Invoke("DropApple", 2f);
+        }
+        else
+            Invoke("DropAppleGold", 2f);
+    }
     void DropApple()
     {
         GameObject apple = Instantiate<GameObject>(Apple_Prefab);
         apple.transform.position = transform.position;
         Invoke("DropApple", secondsBetweenAppleDrops);
+        Start();
     }
 
+    void DropAppleGold()
+    {
+        GameObject apple = Instantiate<GameObject>(Gold_Apple_Prefab);
+        apple.transform.position = transform.position;
+        Invoke("DropAppleGold", secondsBetweenAppleDrops);
+        Start();
+    }
     void Update()
     {
         Vector3 pos = transform.position;
